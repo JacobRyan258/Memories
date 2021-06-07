9cce25adb29bd5415eda1661c94c63e2f1588ff9
Jacob Ryan <jacobryan258@gmail.com>
2021-06-07 12:48:08 -0500

    Update index.js
    password information was visible

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://jacobryan258:Jake&andi741$@memories.w675u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://jacobryan:Jake&andi@memories.w675u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
